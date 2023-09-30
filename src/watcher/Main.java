795369ac50111c484d5eaf8fdf19e452380c488d
Tahircan45 <tahircanakgun@gmail.com>
2023-09-30 19:16:06 +0300

    isimlendirmeler güncellendi

--- src/watcher/Main.java
+++ src/watcher/Main.java
@@ -6,15 +6,15 @@ import java.util.concurrent.*;
 
 public class Main {
 
-    public static HashMap<Character,BlockingQueue<Product>> blockingQueueMap = new HashMap<>();
-    public static final Map<String,Integer> globalProducts = Collections.synchronizedMap(new HashMap<>()) ;
+    public static HashMap<Character, BlockingQueue<Product>> blockingQueueMap = new HashMap<>();
+    public static final Map<String, Integer> globalProducts = Collections.synchronizedMap(new HashMap<>());
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         for (char c = 'A'; c <= 'Z'; ++c) {
-            LinkedBlockingQueue<Product> blockingQueue = new LinkedBlockingQueue<>();
-            blockingQueueMap.put(c,blockingQueue);
-
-            Thread processThread = new Thread(new ProcessThread(blockingQueue,String.valueOf(c)));
+            LinkedBlockingQueue<Product> processQueue = new LinkedBlockingQueue<>();
+            blockingQueueMap.put(c, processQueue);
+            String processName = "Process " + c;
+            Thread processThread = new Thread(new ProcessThread(processQueue,processName));
             processThread.start();
         }
         Thread fileReaderThread = new Thread(new FileReaderThread(blockingQueueMap));
